f407902f6c22f9e311c953bdc6f5c6bfaa4f4549
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2025-04-23 21:03:43 -0300

    fix: Added config to publish

--- src/TrellogServiceProvider.php
+++ src/TrellogServiceProvider.php
@@ -8,9 +8,16 @@ class TrellogServiceProvider extends ServiceProvider
 {
   public function register()
   {
+    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'trellog');
   }
 
   public function boot()
   {
+    if($this->app->runningInConsole()){
+      $this->publishes([
+        __DIR__.'/../config/config.php' => config_path('trellog.php'),
+      ], 'trellog.config');
+
+    }
   }
 }
